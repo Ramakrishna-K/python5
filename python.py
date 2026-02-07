04a85ae58157960d6235380123dec08f6da36df5
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2026-02-07 05:40:39 +0530

    python5 file

---
+++ python.py
@@ -0,0 +1,53 @@
+
+# A modules is a file that  contains ready made python code (function , variables  and classes ) that you reuse.
+
+# used for the mathematical operations.
+"""
+import math
+print(math.sqrt(16))
+print(math.ceil(4.3))
+print(math.floor(4.9))
+print(math.pi)
+"""
+# math -> module name
+# sqrt -> function inside math module
+# 16 is number
+
+
+
+# Used to generate random values (games, OTPs, dice, etc.)
+
+# import random 
+# print(random.randint(1,10))
+"""
+import random
+print(random.randint(1, 10))
+print(random.random())
+print(random.choice([10,20,30]))
+print(random.choice([10,20,30]))   random item from list
+"""
+# randint(a, b) → returns a random integer
+
+# Range is inclusive
+
+# Used to work with date & time
+"""
+import datetime
+
+print(datetime.datetime.now())
+now = datetime.datetime.now()
+print(now.date())
+print(now.time())
+print(now.year)     
+print(now.month)    
+print(now.day)   
+"""
+
+
+# Real-Life Use
+
+# math → calculator apps
+
+# random → OTP, games, lucky draw
+
+# datetime → login time, booking date, logs
